b4ed50db58ef3c8f0dc61f2e268b3ecd500b4afb
Anton Stremousov <stremousoff@gmail.com>
2024-03-18 18:08:10 +0300

    v1

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -31,4 +31,4 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
-        ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
