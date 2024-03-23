f21831a7922258db5e3da1376715e86ec188ce5f
Anton Stremousov <stremousoff@gmail.com>
2024-03-23 17:28:06 +0300

    v4

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
